fcd7bd38b4fc6157f72ef8a5547a35a303394b15
Sanghamitra Satpathy <sanghamitramymail@gmail.com>
2024-10-23 16:11:18 +0530

    updated search books

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const categoryRouter = require("./routes/category-route");
 
 app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
